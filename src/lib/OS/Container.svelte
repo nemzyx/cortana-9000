1894b15269536651397375e731c44d78c7454cf4
ferry-creator <ferryisworking@gmail.com>
2021-07-17 21:36:42 +0200

    Beautiful windows 11 desktop page

---
+++ src/lib/OS/Container.svelte
@@ -0,0 +1,122 @@
+<div class="oscontainer">
+	<div class="desktop">
+		<!-- <button> <img src="svg/outlook.png" /> </button>
+		<button> <img src="svg/MSPaint.png" /> </button> -->
+		<button> <img src="svg/outlook.png" /> <span>Outlook</span> </button>
+		<button> <img src="svg/MSPaint.png" /> <span>Paint</span> </button>
+	</div>
+	<div class="status">
+		<div class="process">
+			<button> <img src="svg/start.svg" /> </button>
+			<button> <img src="svg/outlook.png" /> </button>
+			<button> <img src="svg/MSPaint.png" /> </button>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	$processHeight: 6rem;
+
+	.oscontainer {
+		width: 100vw;
+		height: calc(100vh);
+
+		background-image: url('11.jpeg');
+		background-size: cover;
+		background-position-x: center;
+
+		position: relative;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.desktop {
+		flex: 1;
+		width: 0;
+		height: calc(100% - $processHeight);
+		/* background: purple; */
+		display: flex;
+		flex-direction: column;
+		flex-wrap: wrap;
+		gap: 3.5rem;
+		padding: 2rem;
+
+		button {
+			$desktopIconSize: 6rem;
+			height: $desktopIconSize;
+			width: $desktopIconSize;
+
+			img {
+				height: 100%;
+				padding: 1.1rem;
+				display: inline;
+			}
+			span {
+				color: white;
+				font-size: 1.2rem;
+				position: relative;
+				bottom: 0.4rem;
+			}
+		}
+	}
+
+	.process {
+		background: white;
+		padding: 0 8rem;
+		height: $processHeight;
+		margin: auto;
+		display: flex;
+		border-radius: 10px 10px 0 0;
+		position: absolute;
+		bottom: 0;
+
+		button {
+			width: 7rem;
+
+			img {
+				height: 100%;
+				padding: 1.1rem;
+				display: inline;
+			}
+		}
+
+		&:after {
+			content: '';
+			display: block;
+			background: purple;
+			height: 10px;
+			width: 10px;
+			position: absolute;
+			bottom: calc($processHeight / 3);
+			left: 0;
+			transform: translateX(-100%);
+		}
+	}
+
+	.status {
+		position: relative;
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		height: calc($processHeight / 3);
+		background: white;
+	}
+
+	button {
+		img {
+			transition: 300ms;
+		}
+		&:hover {
+			img {
+				transform: scale(1.1);
+				transition: 180ms;
+			}
+		}
+		&:active {
+			img {
+				transform: scale(1.2);
+				transition: 25ms;
+			}
+		}
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,24 +3,104 @@
 </script>
 
 <script>
+	import { fade } from 'svelte/transition'
 	import { onMount } from 'svelte'
-	import { clickStart, debug, history } from '$lib/state'
-	import Slides from '$lib/Slides.svelte'
-	
-	debug.set(true)
 
-	function AddHistory(key, value, index) {
+	let showGreet = false
+	let greeting = ''
+	let inputText = ''
+	let inputUpdated = false
+	let displayInput = false
+	let speedUp = true
+	let inputField
+	let elem
 
-	}
-		
-	// Store variables
-	let clickStart_tracker
-	clickStart.subscribe((val) => clickStart_tracker = val)
+	onMount(() => {
+		/* Get the documentElement (<html>) to display the page in fullscreen */
+		elem = document.documentElement
 
-	onMount((val) => {	
-		let utterance = new window.SpeechSynthesisUtterance("Hello world!")
-		speechSynthesis.speak(utterance)
+		/* View in fullscreen */
+		function openFullscreen() {
+			console.log(elem.requestFullscreen)
+			if (elem.requestFullscreen) {
+				elem.requestFullscreen()
+			} else if (elem.webkitRequestFullscreen) {
+				/* Safari */
+				elem.webkitRequestFullscreen()
+			} else if (elem.msRequestFullscreen) {
+				/* IE11 */
+				elem.msRequestFullscreen()
+			}
+		}
+
+		openFullscreen()
 	})
+
+	// Default time = time to show the slide
+	function Slide(text, show, time = 1500) {
+		return new Promise((resolve) => {
+			setTimeout(
+				() => {
+					greeting = text
+					showGreet = show
+					resolve('resolved')
+					console.log('Promise ran with: ' + text)
+				},
+				speedUp ? time * 0.1 : time,
+			)
+		})
+	}
+
+	function timeout(ms) {
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				resolve('resolved')
+			}, ms)
+		})
+	}
+
+	function ShowInput() {
+		// inputField.value = ''
+		displayInput = true
+	}
+
+	function HandleInput() {}
+
+	async function AwaitUserInput() {
+		console.log('Awaiting user input.')
+		while (!inputUpdated) await timeout(50)
+		inputUpdated = false
+		displayInput = false
+	}
+
+	async function AwaitPermitCamera() {
+		await Slide('', false, 4000)
+		await Slide("it's just to check your answer", false)
+		await Slide('', false, 6000)
+		await Slide('please allow me to use the camera', false)
+	}
+
+	let run = async () => {
+		await Slide('hello', true)
+		await Slide('', false, 3000)
+		await Slide("let's design your vaccine DNA", true)
+		await Slide('', false, 5000)
+		await Slide("I'll just ask some questions", true)
+		await Slide('', false, 5000)
+		await Slide('I just need access to the camera', true)
+		await AwaitPermitCamera()
+		await Slide('', false, 100)
+		await Slide('what is your age?', true)
+		ShowInput()
+		await AwaitUserInput()
+		await HandleInput()
+		await Slide('', false, 100)
+		await Slide('what is your gender?', true)
+		ShowInput()
+		await AwaitUserInput()
+	}
+
+	run()
 </script>
 
 <svelte:head>
@@ -28,12 +108,24 @@
 </svelte:head>
 
 <section>
-
-	<Slides />
-
-	<div class="process-bar">
-		<a href="#" on:click={() => {clickStart.set(!clickStart_tracker)}}><img src="windows-icon.jpg"/></a>
-	</div>
+	<img class="cortana" src="/cortana.gif" />
+	{#if showGreet}
+		<div class="greet" transition:fade>{greeting}</div>
+	{:else}
+		<div class="greet" />
+	{/if}
+	<input
+		type="text"
+		bind:this={inputField}
+		style="display: {displayInput ? 'block' : 'none'};"
+		name="title"
+		class="form-control"
+		bind:value={inputText}
+		on:change={() => {
+			inputUpdated = true
+			console.log('Input updated')
+		}}
+	/>
 </section>
 
 <style lang="postcss">
@@ -44,9 +136,15 @@
 		align-items: flex-center;
 		place-items: center;
 		min-height: 100vh;
-		background: #0E0E0E;
+		background: #0e0e0e;
 		text-align: center;
-		color: #38ACC1;
+		color: #38acc1;
+
+		.greet {
+			font-size: 6rem;
+			line-height: 6rem;
+			height: 10rem;
+		}
 
 		.process-bar {
 			display: flex;
@@ -55,9 +153,13 @@
 			position: absolute;
 			padding: 1vh;
 
-			img {
-				height: 4vh;
-			}
+		input {
+			background: #ffffff05;
+			border: 1px #e7f3fd5b solid;
+			height: 3rem;
+			width: 30rem;
+			font-size: 2rem;
+			text-align: center;
 		}
 	}
 </style>
