6792fc303bc4c95f262efcc2851f9f1373cf49e9
dr-kranc <esben@eachthing.com>
2021-07-17 02:50:08 +0200

    inputting tests

---
+++ src/routes/ml-test.svelte
@@ -0,0 +1,58 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+	import {QnA} from 'svelteml'
+
+    // const passage = "Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, search engine, cloud computing, software, and hardware. It is considered one of the Big Four technology companies, alongside Amazon, Apple, and Facebook. Google was founded in September 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University in California. Together they own about 14 percent of its shares and control 56 percent of the stockholder voting power through supervoting stock. They incorporated Google as a California privately held company on September 4, 1998, in California. Google was then reincorporated in Delaware on October 22, 2002. An initial public offering (IPO) took place on August 19, 2004, and Google moved to its headquarters in Mountain View, California, nicknamed the Googleplex. In August 2015, Google announced plans to reorganize its various interests as a conglomerate called Alphabet Inc. Google is Alphabet's leading subsidiary and will continue to be the umbrella company for Alphabet's Internet interests. Sundar Pichai was appointed CEO of Google, replacing Larry Page who became the CEO of Alphabet.";
+    // const question = 'Who is the CEO of Google?';
+
+    // // optional
+    // function onAnswer(answers) {
+    //     alert(answers);
+    // }
+
+ </script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section>
+	<h1>YEET your way to success!</h1>
+
+	<h2>You damn boi, here's some ML text:</h2>
+
+    <!-- <QnA passage="{passage}" question="{question}" on:answers="{onAnswer}" /> -->
+
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
