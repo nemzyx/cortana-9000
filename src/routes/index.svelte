d7af5d5c6d935da9e74f5102f29eec1601d56cb9
ferry-creator <ferryisworking@gmail.com>
2021-07-17 02:49:51 +0200

    🚫 deleting some files

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,14 @@
 <script context="module">
-	export const prerender = true;
+	export const prerender = true
+	export const ssr = false
 </script>
 
 <script>
-	import Counter from '$lib/Counter.svelte';
+	import Instacam from 'instacam'
+
+	let cameraRender
+
+	let camera = new Instacam(cameraRender)
 </script>
 
 <svelte:head>
@@ -11,22 +16,8 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
+	Hello
+	<canvas bind:this={cameraRender} />
 </section>
 
 <style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,22 @@
 import preprocess from 'svelte-preprocess'
-import netlify from '@sveltejs/adapter-netlify';
+import netlify from '@sveltejs/adapter-netlify'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: netlify()
+		adapter: netlify(),
+
+		vite: {
+			// optimizeDeps: {
+			// 	include: ['blurhash'],
+			// },
+			// ssr: {
+			// 	// noExternal: ['svelte-image'],
+			// 	noExternal: ['instacam'],
+			// },
+		},
 	},
 
 	preprocess: [
