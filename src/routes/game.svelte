d2878f63f6c889f4297cd615e607f5a9c4c7b33d
ferry-creator <ferryisworking@gmail.com>
2021-07-17 14:26:19 +0200

    🔨 Deployment test with vercel

--- src/routes/game.svelte
+++ src/routes/game.svelte
@@ -1,19 +1,24 @@
 <script>
-    import Camera from '$lib/Camera.svelte'
-    import ML from '$lib/ML.svelte'
+	import Camera from '$lib/Camera.svelte'
+	import ML from '$lib/ML.svelte'
 </script>
 
 <svelte:head>
 	<title>Home</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link
+		rel="stylesheet"
+		href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+		integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+		crossorigin="anonymous"
+	/>
 </svelte:head>
 
 <div class="m-5" style="text-align: center">
-    
-    <h1>Setup vaccine</h1>
+	<h1>Setup vaccine</h1>
 
-    <ML/>
-    <Camera />
-</div>    
+	<ML />
+	<Camera />
+</div>
+
 <style>
 </style>
--- src/routes/ml-test.svelte
+++ src/routes/ml-test.svelte
@@ -1,19 +1,16 @@
 <script context="module">
-	export const prerender = true;
+	export const prerender = true
 </script>
 
 <script>
-	import {QnA} from 'svelteml'
+	// const passage = "Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, search engine, cloud computing, software, and hardware. It is considered one of the Big Four technology companies, alongside Amazon, Apple, and Facebook. Google was founded in September 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University in California. Together they own about 14 percent of its shares and control 56 percent of the stockholder voting power through supervoting stock. They incorporated Google as a California privately held company on September 4, 1998, in California. Google was then reincorporated in Delaware on October 22, 2002. An initial public offering (IPO) took place on August 19, 2004, and Google moved to its headquarters in Mountain View, California, nicknamed the Googleplex. In August 2015, Google announced plans to reorganize its various interests as a conglomerate called Alphabet Inc. Google is Alphabet's leading subsidiary and will continue to be the umbrella company for Alphabet's Internet interests. Sundar Pichai was appointed CEO of Google, replacing Larry Page who became the CEO of Alphabet.";
+	// const question = 'Who is the CEO of Google?';
 
-    // const passage = "Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, search engine, cloud computing, software, and hardware. It is considered one of the Big Four technology companies, alongside Amazon, Apple, and Facebook. Google was founded in September 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University in California. Together they own about 14 percent of its shares and control 56 percent of the stockholder voting power through supervoting stock. They incorporated Google as a California privately held company on September 4, 1998, in California. Google was then reincorporated in Delaware on October 22, 2002. An initial public offering (IPO) took place on August 19, 2004, and Google moved to its headquarters in Mountain View, California, nicknamed the Googleplex. In August 2015, Google announced plans to reorganize its various interests as a conglomerate called Alphabet Inc. Google is Alphabet's leading subsidiary and will continue to be the umbrella company for Alphabet's Internet interests. Sundar Pichai was appointed CEO of Google, replacing Larry Page who became the CEO of Alphabet.";
-    // const question = 'Who is the CEO of Google?';
-
-    // // optional
-    // function onAnswer(answers) {
-    //     alert(answers);
-    // }
-
- </script>
+	// // optional
+	// function onAnswer(answers) {
+	//     alert(answers);
+	// }
+</script>
 
 <svelte:head>
 	<title>Home</title>
@@ -24,8 +21,7 @@
 
 	<h2>You damn boi, here's some ML text:</h2>
 
-    <!-- <QnA passage="{passage}" question="{question}" on:answers="{onAnswer}" /> -->
-
+	<!-- <QnA passage="{passage}" question="{question}" on:answers="{onAnswer}" /> -->
 </section>
 
 <style>
